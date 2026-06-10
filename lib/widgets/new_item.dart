fe9cb4568941b2639bd490cf26e23a91f6c6523a
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-10 18:07:05 +0300

    added form structure

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -12,7 +13,60 @@ class _NewItemState extends State<NewItem> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Add New Item')),
-      body: const Center(child: Text('This is the Add New Item screen.')),
+      body: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              maxLength: 50,
+              decoration: const InputDecoration(label: Text('Name')),
+              validator: (value) =>
+                  value!.trim().isEmpty ? 'Please enter a name' : null,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(label: Text('Quantity')),
+                    initialValue: '1',
+                    validator: (value) {
+                      if (value == null ||
+                          value.trim().isEmpty ||
+                          int.tryParse(value) == null ||
+                          int.parse(value) <= 0) {
+                        return 'Please enter a valid quantity';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: DropdownButtonFormField(
+                    items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 6),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        ),
+                    ],
+                    onChanged: (value) {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
