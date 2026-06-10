b1a0fee2b2bc26b287ec880b9f0f62c063c61249
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-10 16:46:29 +0300

    added + sign

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
           surface: const Color.fromARGB(255, 42, 51, 59),
         ),
       ),
-      
+      home: const GroceryList(),
     );
   }
 
