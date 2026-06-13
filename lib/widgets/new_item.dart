95b5e45c3e2c5ad07e46da76c732b1f57f71bb99
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-13 09:31:33 +0300

    connecting to fire base

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import "package:http/http.dart" as http;
+import 'dart:convert';
 import "package:flutter/material.dart";
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
@@ -20,6 +22,21 @@ class _NewItemState extends State<NewItem> {
     _formKey.currentState!.validate();
     {
       _formKey.currentState!.save();
+
+      final url = Uri.https(
+        "flutter-prep-a74c4-default-rtdb.firebaseio.com/",
+        "shopping-list.json",
+      );
+
+      http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({
+          "name": _enteredName,
+          "quantity": _enteredQuantity,
+          "category": _selectedCategory.title,
+        }),
+      );
 
       Navigator.of(context).pop(
         GroceryItem(
